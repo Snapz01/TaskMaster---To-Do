82c8d5da1ef78d95ee6c04365b162f524deef5d4
Snapz01 <timmy.larsson@hotmail.com>
2024-07-11 22:52:19 +0200

    Task 8 - PeopleService

---
+++ TaskMaster.Tests/PeopleService.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using TaskMaster.Data;
+using TaskMaster.Models;
+
+namespace TaskMaster.Tests
+{
+    public class PeopleServiceTests
+    {
+        [Fact]
+        public void Size_ReturnZero()
+        {
+            var service = new PeopleService();
+
+            int size = service.Size();
+
+            Assert.Equal(0, size);
+        }
+
+        [Fact]
+        public void Peron_equalSamePerson()
+        {
+            var service = new PeopleService();
+            var person = service.CreatePerson("Timmy Larsson", 30);
+
+            var foundPerson = service.FindById(person.Id);
+
+            Assert.Equal(person, foundPerson);
+        }
+
+        [Fact]
+        public void Clear_ShouldRemoveAllPeople()
+        {
+            var service = new PeopleService();
+            service.CreatePerson("John Doe", 30);
+
+            service.Clear();
+            var allPeople = service.FindAll();
+
+            Assert.Empty(allPeople);
+        }
+    }
+}
---
+++ TaskMaster/Data/PeopleService.cs
@@ -0,0 +1,40 @@
+﻿using TaskMaster.Models;
+
+namespace TaskMaster.Data
+{
+    public class PeopleService
+    {
+        private static Person[] people = new Person[0];
+
+        public int Size()
+        {
+            return people.Length;
+        }
+
+        public Person[] FindAll()
+        {
+            return people;
+        }
+
+        public Person FindById(int personId)
+        {
+            return people.FirstOrDefault(p => p.Id == personId);
+        }
+
+        public Person CreatePerson(string name, int age)
+        {
+            int personId = PersonSequencer.NextPersonId();
+            Person newPerson = new Person { Id = personId, Name = name, Age = age };
+
+            Array.Resize(ref people, people.Length + 1);
+            people[people.Length - 1] = newPerson;
+
+            return newPerson;
+        }
+
+        public void Clear()
+        {
+            people = new Person[0];
+        }
+    }
+}

5bca1a7dc4ae86b321a9db60d096c8d413491ae3
Snapz01 <timmy.larsson@hotmail.com>
2024-07-12 00:53:51 +0200

    task 9 - TodoService

---
+++ TaskMaster.Tests/TodoServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using TaskMaster.Data;
+using TaskMaster.Models;
+
+namespace TaskMaster.Tests
+{
+    public class TodoServiceTests
+    {
+        [Fact]
+        public void Size_ReturnZero()
+        {
+            var service = new TodoService();
+
+            int size = service.Size();
+
+            Assert.Equal(0, size);
+        }
+
+
+        [Fact]
+        public void Id_ShouldReturnTodo()
+        {
+            var service = new TodoService();
+            var todo = service.CreateTodo("Test todo");
+
+            var foundTodo = service.FindById(todo.Id);
+
+            Assert.Equal(todo, foundTodo);
+        }
+
+        [Fact]
+        public void Clear_ShouldRemoveAllTodos()
+        {
+            var service = new TodoService();
+            service.CreateTodo("Test todo");
+
+            service.Clear();
+            var allTodos = service.FindAll();
+
+            Assert.Empty(allTodos);
+        }
+    }
+}
---
+++ TaskMaster/Data/TodoService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using TaskMaster.Models;
+
+namespace TaskMaster.Data
+{
+    public class TodoService
+    {
+        private static Todo[] todos = new Todo[0];
+
+        public int Size()
+        {
+            return todos.Length;
+        }
+
+        public Todo[] FindAll()
+        {
+            return todos;
+        }
+
+        public Todo FindById(int todoId)
+        {
+            return todos.FirstOrDefault(t => t.Id == todoId);
+        }
+
+        public Todo CreateTodo(string description)
+        {
+            int todoId = TodoSequencer.NextTodoId();
+            Todo newTodo = new Todo(todoId, description);
+
+            Array.Resize(ref todos, todos.Length + 1);
+            todos[todos.Length - 1] = newTodo;
+
+            return newTodo;
+        }
+
+        public void Clear()
+        {
+            todos = new Todo[0];
+        }
+    }
+}

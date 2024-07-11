b756c218fa195b437d3e8461f969b9cea20e1293
Snapz01 <timmy.larsson@hotmail.com>
2024-07-11 14:53:28 +0200

    task 7 ( TodoSequencer )

---
+++ TaskMaster.Tests/TodoSequencerTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using TaskMaster.Data;
+
+namespace TaskMaster.Tests
+{
+    public class TodoSequencerTests
+    {
+        [Fact]
+        public void ShouldIncrementAndReturnNextId()
+        {
+            //reset
+            TodoSequencer.Reset();
+            //get id
+            int firstId = TodoSequencer.NextTodoId();
+            int secondId = TodoSequencer.NextTodoId();
+            //verify
+            Assert.Equal(1, firstId);
+            Assert.Equal(2, secondId);
+        }
+
+        [Fact]
+        public void Reset_ShouldSetTodoIdToZero()
+        {
+            //get id
+            TodoSequencer.NextTodoId();
+            TodoSequencer.NextTodoId();
+            //reset and next id
+            TodoSequencer.Reset();
+            int nextId = TodoSequencer.NextTodoId();
+            //verify
+            Assert.Equal(1, nextId);
+        }
+    }
+}
